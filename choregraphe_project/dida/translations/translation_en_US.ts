df6d5e359963a0645d984d874ab1108f538b46f3
Marua Eduarda Trevizane Buonafina <mebuonafina@gmail.com>
2025-09-30 22:44:20 -0300

    Iniciando história 2

--- choregraphe_project/dida/translations/translation_en_US.ts
+++ choregraphe_project/dida/translations/translation_en_US.ts
@@ -1,3 +1,13 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <!DOCTYPE TS>
-<TS version="2.1" language="en_US"/>
+<TS version="2.1" language="en_US">
+    <context>
+        <name>behavior_1/behavior.xar:/Inicio Aprest/behavior_layer1/keyframe1/Histórias/behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
+</TS>
