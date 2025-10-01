eb6f7c91d38c13e346d0c5809da78c28aa6c5eae
Marua Eduarda Trevizane Buonafina <mebuonafina@gmail.com>
2025-09-30 23:48:05 -0300

    História 2 finalizada

--- choregraphe_project/dida/translations/translation_en_US.ts
+++ choregraphe_project/dida/translations/translation_en_US.ts
@@ -10,4 +10,31 @@
             <translation>Hello</translation>
         </message>
     </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Inicio Aprest/behavior_layer1/keyframe1/Histórias/behavior_layer1/keyframe1/História 2/behavior_layer1/keyframe1/Animated Say</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Inicio Aprest/behavior_layer1/keyframe1/Histórias/behavior_layer1/keyframe1/História 2/behavior_layer1/keyframe1/Animated Say (1)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
+    <context>
+        <name>behavior_1/behavior.xar:/Inicio Aprest/behavior_layer1/keyframe1/Histórias/behavior_layer1/keyframe1/História 2/behavior_layer1/keyframe1/Animated Say (2)</name>
+        <message>
+            <location filename="behavior_1/behavior.xar" line="0"/>
+            <source>Hello</source>
+            <comment>Text</comment>
+            <translation>Hello</translation>
+        </message>
+    </context>
 </TS>
